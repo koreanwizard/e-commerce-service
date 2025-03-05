d432111db20cff49711b531d5ccf8e1a10432d57
koreanwizard <harry992178@gmail.com>
2025-03-05 23:36:19 +0900

    a service for deleting user; implements Command interface

---
+++ src/main/java/com/madebywizard/ecommerce/user/DeleteUserService.java
@@ -0,0 +1,16 @@
+package com.madebywizard.ecommerce.user;
+
+import com.madebywizard.ecommerce.Command;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteUserService implements Command<Void, String> { // temporarily, input is void and output is string
+
+    @Override
+    public ResponseEntity<String> execute(Void input) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("delete user");
+
+    }
+}
