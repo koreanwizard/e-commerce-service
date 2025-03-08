b7822aa55d715b15324c66123134f04fa6215de5
koreanwizard <harry992178@gmail.com>
2025-03-08 13:28:44 +0900

    add a UserValidator's 'execute' method to validate updating user information

--- src/main/java/com/madebywizard/ecommerce/user/services/UpdateUserService.java
+++ src/main/java/com/madebywizard/ecommerce/user/services/UpdateUserService.java
@@ -1,10 +1,12 @@
 package com.madebywizard.ecommerce.user.services;
 
 import com.madebywizard.ecommerce.Command;
+import com.madebywizard.ecommerce.exceptions.UserNotFoundException;
 import com.madebywizard.ecommerce.user.UserRepository;
 import com.madebywizard.ecommerce.user.model.UpdateUserCommand;
 import com.madebywizard.ecommerce.user.model.User;
 import com.madebywizard.ecommerce.user.model.UserDTO;
+import com.madebywizard.ecommerce.user.validators.UserValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UpdateUserService implements Command<UpdateUserCommand, UserDTO> { // temporarily, input is void and output is string
+public class UpdateUserService implements Command<UpdateUserCommand, UserDTO> { // input an UpdatUserCommand object and output UserDTO obj
 
 
     private final UserRepository userRepository;
@@ -24,19 +26,22 @@ public class UpdateUserService implements Command<UpdateUserCommand, UserDTO> { 
 
     @Override
     public ResponseEntity<UserDTO> execute(UpdateUserCommand command) {
+        /*
+        validate new user's information by using 'UserValidator' 'execute' method by passing current user object
+        'save' JpaRepository method saves but also updates the data
+         */
 
         Optional<User> userOptional = userRepository.findById(command.getId());
         if (userOptional.isPresent()) {
             User user = command.getUser();
             user.setId(command.getId());
+
+            UserValidator.execute(user); // validator
+
             userRepository.save(user);
             return ResponseEntity.ok(new UserDTO(user));
         }
 
-        /*
-        an exception handler will be added in here
-        */
-
-        return null;
+        throw new UserNotFoundException();
     }
 }
