ea48bb10702ddb2775d2825a1f38db4a28abb414
koreanwizard <harry992178@gmail.com>
2025-03-08 13:01:05 +0900

    add a new spring data jpa method that search User object with a specific last name

--- src/main/java/com/madebywizard/ecommerce/user/UserRepository.java
+++ src/main/java/com/madebywizard/ecommerce/user/UserRepository.java
@@ -5,6 +5,15 @@ import com.madebywizard.ecommerce.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // spring data jpa
+    List<User> findByLastNameContaining(String lastName);
+
+    // we can also use JPQL to create a customized query but in this project we will just use the Jpa.
+
+
 }
