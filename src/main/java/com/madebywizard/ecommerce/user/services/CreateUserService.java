d2f955c72a369dc247f4095f4e25ef5bd6550ed3
koreanwizard <harry992178@gmail.com>
2025-03-08 13:12:35 +0900

    the UserValidator's 'execute' method has been added to validate user's input information

--- src/main/java/com/madebywizard/ecommerce/user/services/CreateUserService.java
+++ src/main/java/com/madebywizard/ecommerce/user/services/CreateUserService.java
@@ -4,6 +4,7 @@ import com.madebywizard.ecommerce.Command;
 import com.madebywizard.ecommerce.user.UserRepository;
 import com.madebywizard.ecommerce.user.model.User;
 import com.madebywizard.ecommerce.user.model.UserDTO;
+import com.madebywizard.ecommerce.user.validators.UserValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -21,9 +22,13 @@ public class CreateUserService implements Command<User, UserDTO> { // now the in
     @Override
     public ResponseEntity<UserDTO> execute(User user) {
         /*
+        validate new user's information by using 'UserValidator' 'execute' method by passing current user object
         call 'save' method which is the JpaRepository method,
         and pass the newly saved 'User' object into the new 'UserDTO' object
          */
+
+        UserValidator.execute(user); // validator
+
         User savedUser = userRepository.save(user);
         System.out.println(savedUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(new UserDTO(savedUser));
