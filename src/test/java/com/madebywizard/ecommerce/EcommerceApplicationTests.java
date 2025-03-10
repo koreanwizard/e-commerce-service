130da864f92dad1e21301eb224551acb877daf9d
koreanwizard <harry992178@gmail.com>
2025-03-10 10:40:10 +0900

    add some unit tests and an explanation file

--- src/test/java/com/madebywizard/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/madebywizard/ecommerce/EcommerceApplicationTests.java
@@ -8,6 +8,8 @@ class EcommerceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// a sanity check (this test should always pass)
+		// if this fails -> there is some bugs or compile issue on my project
 	}
 
 }
