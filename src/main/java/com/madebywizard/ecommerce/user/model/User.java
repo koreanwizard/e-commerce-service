4bc5bc70c899f90b227580537e51bca751150f79
koreanwizard <harry992178@gmail.com>
2025-03-26 01:04:22 +0900

    add some attributes and explanations about User object

--- src/main/java/com/madebywizard/ecommerce/user/model/User.java
+++ src/main/java/com/madebywizard/ecommerce/user/model/User.java
@@ -3,46 +3,114 @@ package com.madebywizard.ecommerce.user.model;
 
 import com.madebywizard.ecommerce.item.model.Item;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.Objects;
 
 
+// A User entity, the table name is "user"
 @Entity
 @Table(name = "user")
 public class User {
 
+
+    /*
+    a primary key and it's automatically generated
+    DATABASE: INT
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "first_name")
+
+    /*
+    a user's id. should be alphanumerical (will be handled form UserValidator) (required)
+    DATABASE: VARCHAR(255) NOT NULL
+     */
+    @NotNull(message = "user id is required")
+    @Column(name = "user_Id", nullable = false)
+    private String userId;
+
+
+//     user password will be added here in the future (will be encrypted)
+//     DATABASE: VARCHAR(255)
+//     @NotNull(message = "user password is required")
+//     @Column(name = "user_password", nullable = false)
+//     private String userPassword;
+
+
+    /*
+    a first name of the user. should be alphabetical (will be handled from UserValidator) (required)
+    DATABASE: VARCHAR(255) NOT NULL
+     */
+    @NotNull(message = "first name is required")
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
+
+    /*
+    a last name of the user. should be alphabetical (will be handled from UserValidator) (required)
+    DATABASE: VARCHAR(255) NOT NULL
+     */
+    @NotNull(message = "last name is required")
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "gender")
+
+
+    /*
+    user's gender. should be alphabetical (will be handled from UserValidator) (required)
+    DATABASE: VARCHAR(255) NOT NULL
+     */
+    @NotNull(message = "gender is required")
+    @Column(name = "gender", nullable = false)
     private String gender;
 
-    @Column(name = "email")
+
+
+    /*
+    user's email. should be an email format: example@example.com (will be handled from UserValidator) (required)
+    DATABASE: VARCHAR(255) NOT NULL UNIQUE
+     */
+    @NotNull(message = "email is required")
+    @Email(message = "invalid email format")
+    @Column(name = "email", nullable = false)
     private String email;
 
+
+    /*
+    user's cart. should be one-to-one relationship with user because user can only have one cart
+    and each cart can be associated with one user.
+    cascade: CascadeType.ALL -> if user entity makes an operation the related entity (cart) also do the same operation.
+             ex: if user's deleted, the cart will also be deleted
+     */
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "cart_id")
     private Cart cart;
 
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name="user_id")
+    /*
+    user's addresses (delivery locations). should be many-to-many relationship with user because user can have multiple
+    addresses and each address can be associated with multiple user (address can be shared if user1 and user2 are related)
+    create a junction table with user_id (Foreign Key for User) and address_id (Foreign Key for Address)
+     */
+    @ManyToMany
+    @JoinTable(
+            name="user_address",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id")
+    )
     private List<Address> addresses;
 
 
     public User() {}
 
-    public User(Integer id, String firstName, String lastName, String gender, String email, Cart cart, List<Address> addresses) {
+    public User(Integer id, String userId, String firstName, String lastName, String gender, String email, Cart cart, List<Address> addresses) {
         this.id = id;
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
@@ -57,6 +125,14 @@ public class User {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getFirstName() {
@@ -117,6 +193,7 @@ public class User {
         }
         User user = (User) o;
         return Objects.equals(this.id, user.id) &&
+                Objects.equals(this.userId, user.userId) &&
                 Objects.equals(this.firstName, user.firstName) &&
                 Objects.equals(this.lastName, user.lastName) &&
                 Objects.equals(this.gender, user.gender) &&
@@ -129,6 +206,7 @@ public class User {
     public int hashCode() {
         return Objects.hash(
                 this.id,
+                this.userId,
                 this.firstName,
                 this.lastName,
                 this.gender,
@@ -141,6 +219,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" + "id=" + this.id +
+                ", userId='" + this.userId + "'" +
                 ", firstName='" + this.firstName + "'" +
                 ", lastName='" + this.lastName + "'" +
                 ", gender='" + this.gender + "'" +
