7a0a1eab469c667de304bed1d01a477aca9d16f7
koreanwizard <harry992178@gmail.com>
2025-03-25 21:18:48 +0900

    organized the ItemValidator.validate method. add some explanation to the text file

--- src/main/java/com/madebywizard/ecommerce/item/validators/ItemValidator.java
+++ src/main/java/com/madebywizard/ecommerce/item/validators/ItemValidator.java
@@ -3,112 +3,42 @@ package com.madebywizard.ecommerce.item.validators;
 import com.madebywizard.ecommerce.exceptions.ErrorMessages;
 import com.madebywizard.ecommerce.exceptions.ItemNotValidException;
 import com.madebywizard.ecommerce.item.model.Item;
-import com.madebywizard.ecommerce.item.model.ItemSize;
-import com.madebywizard.ecommerce.item.model.ItemType;
+
 import org.springframework.util.StringUtils;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+import java.math.BigDecimal;
 
 public class ItemValidator {
 
     private ItemValidator() {}
 
-    public static void execute(Item item) {
+    public static void validate(Item item) {
 
-
-        // if the item name is not written ex: "", null, " ", "     "
+        // if the item name is not written ex: "", " "
+        // if item name is not written (or an empty space) OR not an alphanumeric
         // throw an INVALID_ITEM_NAME exception
-        if (!StringUtils.hasText(item.getItemName()) || !item.getItemName().matches("[a-zA-Z ]+")) {
+        if (!StringUtils.hasText(item.getItemName()) || !item.getItemName().matches("[a-zA-Z0-9 ]+")) {
             throw new ItemNotValidException(ErrorMessages.INVALID_ITEM_NAME.getMessage());
         }
 
-        // if the item color name is not written ex: "", null, " ", "     " OR if it's not alphabetical
-        // throws an INVALID_ITEM_COLOR
+
+        // if the item color name is not written ex: "", " " OR not alphabetical
+        // throws an INVALID_ITEM_COLOR exception
         if (!StringUtils.hasText(item.getItemColor()) || !item.getItemColor().matches("[a-zA-Z ]+")) {
             throw new ItemNotValidException(ErrorMessages.INVALID_ITEM_COLOR.getMessage());
         }
 
 
-        // check if user input a wrong type or size of item
-        // it seems I should create a custom deserializer to handle exceptions
-        // the postman gives deserialize error: Cannot deserialize....
-        // so I think these if-statement validators not working well -> this will be updated in the future
-
-        if (!isValidEnum("type", item.getItemType().toString())) {
-            throw new ItemNotValidException(ErrorMessages.INVALID_TYPE.getMessage());
+        // if the item price <= 0, throws an INVALID_ITEM_PRICE exception
+        // if the price is less than or equal to 0 the 'compareTo(BigDecimal.ZERO)' will return -1 or 0
+        if (item.getItemPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ItemNotValidException(ErrorMessages.INVALID_ITEM_PRICE.getMessage());
         }
 
-        if (!isValidEnum("size", item.getItemSize().toString())) {
-            throw new ItemNotValidException(ErrorMessages.INVALID_SIZE.getMessage());
-        }
 
-//        if (!isAlphabetic(item.getItemName()) || !isAlphabetic(item.getItemColor())) {
-//            throw new ItemNotValidException(ErrorMessages.ALPHABETIC_REQUIRED.getMessage());
-//        }
-
-        if (item.getRemainingItem() < 0) {
-            throw new ItemNotValidException(ErrorMessages.INVALID_REMAINING_ITEM.getMessage());
+        // if the quantity is less than 0, throws an INVALID_ITEM_QUANTITY exception
+        if (item.getQuantity() < 0) {
+            throw new ItemNotValidException(ErrorMessages.INVALID_ITEM_QUANTITY.getMessage());
         }
     }
-
-
-    public static boolean isValidEnum(String category, String element) {
-
-        if (category.equals("type")) {
-            try {
-                ItemType.valueOf(element);
-                return true;
-            } catch (IllegalArgumentException e) {
-                return false;
-            }
-        } else {
-            try {
-                ItemSize.valueOf(element);
-                return true;
-            } catch (IllegalArgumentException e) {
-                return false;
-            }
-        }
-    }
-
-
-
-//    public static boolean isAlphabetic(String word) {
-//
-//        for (int i = 0; i < word.length(); i++) {
-//            if (!Character.isLetter(word.charAt(i))) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-
-
-
-
-
-
-
-
-
-//    public static boolean isAlphanumeric(String word) {
-//        for (int i = 0; i < word.length(); i++) {
-//            if (!Character.isLetterOrDigit(word.charAt(i))) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//    public static boolean isValidDate(LocalDate date) { // not sure this method is working will be fixed in the future
-//        try {
-//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-//            LocalDate.parse(date.toString(), formatter); // if LocalDate.parse raise an DateTimeParseException
-//            return true;
-//        } catch (DateTimeParseException exception) { // catch and return false to make sure our input date is invalid.
-//            return false;
-//        }
-//    }
 }
