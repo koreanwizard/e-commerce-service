b3bd096e852b34fbd412c2d7b88102030e83f4a0
koreanwizard <harry992178@gmail.com>
2025-03-29 19:29:35 +0900

    add new exception handlers for User entity

--- src/main/java/com/madebywizard/ecommerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/madebywizard/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -19,6 +19,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
     }
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(UserNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotValidException(UserNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
 
 
     // add additional exceptions here and handle it
